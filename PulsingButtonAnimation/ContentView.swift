8a46457f7fbc8d6d73305a84d956ff0aed2ad7e1
wreckss <46939881+Wreckss@users.noreply.github.com>
2020-08-04 12:13:51 -0600

    Completed pulsing button
    Button animation without action

--- PulsingButtonAnimation/ContentView.swift
+++ PulsingButtonAnimation/ContentView.swift
@@ -5,12 +5,37 @@
 //  Created by taco on 8/4/20.
 //  Copyright © 2020 Wrecks. All rights reserved.
 //
+/*
+ https://www.hackingwithswift.com/books/ios-swiftui/customizing-animations-in-swiftui
+ */
+
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount: CGFloat = 1
+    
     var body: some View {
-        Text("Hello, World!")
+        Button("Tap here") {
+            //do some stuff
+        }
+        .padding(50)
+        .background(Color.blue)
+        .foregroundColor(Color.white)
+        .clipShape(Circle())
+        .overlay(
+            Circle()
+                .stroke(Color.blue)
+                .scaleEffect(animationAmount)
+                .opacity(Double(2 - animationAmount))
+                .animation(
+                    Animation.easeInOut(duration: 1)
+                        .repeatForever(autoreverses: false)
+                )
+        )
+            .onAppear {
+                self.animationAmount = 2
+        }
     }
 }
 
